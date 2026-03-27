5566b0a1e32b980c7ae44d1f5ca78968657d6877
ValentinoGaravaglia <valentinogaravaglia7@gmail.com>
2026-03-27 10:38:23 -0300

    Add files via upload

--- casting.py
+++ casting.py
@@ -1,3 +1,27 @@
 def casting():
     """Lee precio, descuento y cantidad como texto y calcula el precio con descuento y el total."""
+
+    precio = input()
+
+    print("Precio: "+precio)
+
+    descuento = input()
+
+    print("Descuento: "+descuento)
+
+    precio_descuento = int(precio) - float(descuento)
+
+    print("Precio con descuento: " + str(precio_descuento))
+
+    cantidad = input()
+
+    total = precio_descuento * int(cantidad)
+
+    print("Total: " + str(total))
+
+
     pass
+
+
+
+
